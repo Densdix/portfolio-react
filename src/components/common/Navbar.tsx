4f7af0283f0e4efa615c72e8a761a46113261331
Densdix <dixfun333333@gmail.com>
2023-06-29 10:46:37 +0300

    got rid of btnRef

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -7,17 +7,17 @@ import s from './Navbar.module.css'
 const Navbar = () => {
 
     const [darkMode, setDarkMode] = useState('light')
-    const btnRef = useRef<HTMLButtonElement>(null)
+    // const btnRef = useRef<HTMLButtonElement>(null)
     useEffect(() => {
-      if(darkMode === 'dark'){
-        document.body.classList.add('dark')
-        btnRef.current?.classList.add(s['dark-mode-btn--active'])
-      }else{
-        document.body.classList.remove('dark')
-        btnRef.current?.classList.remove(s['dark-mode-btn--active'])
-      }
+        if (darkMode === 'dark') {
+            document.body.classList.add('dark')
+            // btnRef.current?.classList.add(s['dark-mode-btn--active'])
+        } else {
+            document.body.classList.remove('dark')
+            // btnRef.current?.classList.remove(s['dark-mode-btn--active'])
+        }
     }, [darkMode])
-    
+
 
     const toggleDarkMode = () => {
         setDarkMode((currentValue) => {
@@ -25,22 +25,26 @@ const Navbar = () => {
         })
     }
 
+
+
     return (
         <>
             <nav className="py-[20px] px-0 bg-[var(--nav-bg)] border border-solid border-[var(--nav-border)] tracking-normal">
                 <div className="max-w-[1200px] my-0 mx-auto p-0 px-[15px]">
                     <div className="flex justify-end max-sm:justify-between items-center gap-x-[30px] gap-y-[20px] flex-wrap font-poppins">
-                    <NavLink to="/" className="mr-auto text-[var(--nav-text)] text-[24px] font-poppins"><strong>Dev</strong> portfolio</NavLink>
+                        <NavLink to="/" className="mr-auto text-[var(--nav-text)] text-[24px] font-poppins"><strong>Dev</strong> portfolio</NavLink>
 
-                        <button ref={btnRef} onClick={toggleDarkMode} className={s['dark-mode-btn']}>
+                        <button
+                            onClick={toggleDarkMode}
+                            className={darkMode === 'dark' ? `${s['dark-mode-btn']} ${s['dark-mode-btn--active']}` : s['dark-mode-btn']}>
                             <img src={sunImg} alt="Light mode" className={s['dark-mode-btn__icon']} />
                             <img src={moonImg} alt="Dark mode" className={s['dark-mode-btn__icon']} />
                         </button>
 
                         <ul className="flex flex-wrap gap-y-[10px] gap-x-[40px] items-center text-[16px] font-medium">
-                            <li><NavLink to='/' className={({isActive}) => isActive ? 'text-indigo-500' : 'text-[var(--nav-text)]'}>Home</NavLink></li>
-                            <li><NavLink to="/projects" className={({isActive}) => isActive ? 'text-indigo-500' : 'text-[var(--nav-text)]'}>Projects</NavLink></li>
-                            <li><NavLink to="/contacts" className={({isActive}) => isActive ? 'text-indigo-500' : 'text-[var(--nav-text)]'}>Contacts</NavLink></li>
+                            <li><NavLink to='/' className={({ isActive }) => isActive ? 'text-indigo-500' : 'text-[var(--nav-text)]'}>Home</NavLink></li>
+                            <li><NavLink to="/projects" className={({ isActive }) => isActive ? 'text-indigo-500' : 'text-[var(--nav-text)]'}>Projects</NavLink></li>
+                            <li><NavLink to="/contacts" className={({ isActive }) => isActive ? 'text-indigo-500' : 'text-[var(--nav-text)]'}>Contacts</NavLink></li>
                         </ul>
                     </div>
                 </div>
